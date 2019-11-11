e834be8da22ce95317dad45a8adc828a2adecc02
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-11-11 18:28:29 +0100

    associations finalized

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
+  has_many :reviews
+  has_many :timeslots
   validates :name, presence: true
   validates :category, presence: true
   validates :description, length: { minimum: 10 }
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,5 +1,6 @@
 class Timeslot < ApplicationRecord
   belongs_to :course
+  has_many :appointments
   validates :date, presence: true
   validates :time, presence: true
 end
