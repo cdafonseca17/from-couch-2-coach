8ef8049c8d11a1237a39ea6c4fe68313f6a9a430
Steven Lund <steve.a.lund@gmail.com>
2019-11-12 11:32:15 +0100

    added reviews controller create new

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
   def create
+    @course = Course.find(params[:course_id])
+    @review = Review.new(review_params)
+    @review.course = @course
+    @review.save
+    redirect_to course_path(@course)
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:stars, :description)
+  end
+end
+
+def new
+  @course = Course.find(params[:course_id])
+  @review = Review.new
 end
