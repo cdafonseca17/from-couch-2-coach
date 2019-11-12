f90909b65580688c6e5ad020f446908a69e9b9f2
Steven Lund <steve.a.lund@gmail.com>
2019-11-12 15:13:16 +0100

    added reviews controller, updated courses show view, added avg ratings to show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'reviews/create'
   root to: 'pages#home'
-    resources :courses
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+    resources :courses do
+       resources :reviews, only: [ :new, :create ]
+     end
+   end
