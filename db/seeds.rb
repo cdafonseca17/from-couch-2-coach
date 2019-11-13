480c0249d6126f7f5be04f40b9ebe95165061082
Steven Lund <steve.a.lund@gmail.com>
2019-11-13 15:45:21 +0100

    changed 1.times to 5.times in timeslots seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,7 +82,7 @@ end
       )
     review.save!
   end
-  1.times do
+  5.times do
     timeslot = Timeslot.new(
       date: Faker::Date.in_date_period(month: 12),
       time: ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"].sample,
