752601d1568c5a77ab960462f5a2656589ee7618
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-11-13 14:56:54 +0100

    bugs fixed

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
-  has_many :reviews
-  has_many :timeslots
+  has_many :reviews, dependent: :destroy
+  has_many :timeslots, dependent: :destroy
   validates :name, presence: true
   validates :category, presence: true
   validates :description, length: { minimum: 10 }
