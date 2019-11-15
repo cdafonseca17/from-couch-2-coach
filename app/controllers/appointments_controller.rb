d9ddc5c73aff38a554673e4af8375e252dad5567
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-11-15 13:53:45 +0100

    search and timeslots fixed

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,30 +1,32 @@
 class AppointmentsController < ApplicationController
   before_action :set_course
-  def newclass AppointmentsController < DelegateClass(ParentClass)
-    def initialize(args)
-      super(del_obj)
+  # def newclass AppointmentsController < DelegateClass(ParentClass)
+  #   def initialize(args)
+  #     super(del_obj)
+  #   end
+  # end
 
-
-    end
-
-
-  end
+  def new
     @appointment = Appointment.new
     @timeslots = Timeslot.where(course_id: params[:course_id])
   end
 
   def create
-    @appointment = Appointment.new(params_create)
-    @appointment.user = current_user
-    authorize @appointment
-
-    if @appointment.save
-      redirect_to managecourses_path
-    else
-      @timeslots = Timeslot.where(course_id: params[:course_id])
-      render :new
-      # p @appointment.errors
+    params["appointment"]["timeslot"].filter { |i| !i.empty? }.each do |t|
+      @appointment = Appointment.new(user: current_user, timeslot_id: t.to_i)
+      @appointment.user = current_user
+      authorize @appointment
+      @appointment.save!
     end
+    # @appointment.timeslot = Timeslot.find()
+    #
+    # if @appointment.save
+    #   redirect_to course_path(@course)
+    # else
+    #   @timeslots = Timeslot.where(course_id: params[:course_id])
+    #   render :new
+    #   # p @appointment.errors
+    # end
   end
 
 private
