e7a30a55e731fea9d78ecd4bb14a0dc5f2af6b65
Adam Dios <adam.dios@gmail.com>
2019-11-15 16:47:09 +0100

    div added back to its spot

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :courses
   has_many :appointments
   # validates :username, uniqueness: true
-  validates :password, length: { within: 6..40 }
+  # validates :password, length: { within: 6..40 }
   validates :email, presence: true
   # validates :phone_number, presence: true
   validates :first_name, presence: true
